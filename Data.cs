6f19c771936f4bccc6223b4fb63c8c610a1dea5d
Hans <hans@win64.se>
2023-01-27 21:10:53 +0100

    Add files via upload

---
+++ Data.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace MyBogus
+{
+    public class Data<T>
+    {
+        public IList<T> List = new List<T>();
+        public const string Characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+        public static List<string> Domains() => new()
+        {
+            "@hotmail.com",
+            "@hotmail.se",
+            "@hotmail.co.uk",
+            "@live.com",
+            "@live.se",
+            "@gmail.com",
+            "@facebook.com",
+            "@mail.com",
+            "@yahoo.com",
+            "@aol.com",
+            "@gmx.com",
+            "@gmx.net",
+            "@mac.com",
+            "@outlook.com",
+            "@msn.com",
+            "@outlook.com",
+            "@comcast.net",
+            "@verizon.net"
+        };
+    }
+}
---
+++ MyBogus.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace MyBogus
+{
+    /*
+     *  Autogenerate random emails
+     */
+    
+    public class MyBogus
+    {
+        public static async Task<IList<string>> RandomEmailAddress(int count, int length)
+        {
+            // Initialize our list to type string
+            var lst = new Data<string>().List;
+
+            // Create our random object
+            var rnd = new Random();
+
+            // Get list of characters
+            var chr = Data<string>.Characters;
+
+            // Get domain names
+            var dmn = Data<List<string>>.Domains();
+
+            // Build up the list of email addresses
+            for (var i = 0; i < count; i++)
+            {
+                lst.Add(
+                    // Add random data before the @
+                    new string(Enumerable.Repeat(chr, length).Select(s => s[rnd.Next(s.Length)]).ToArray()) +
+
+                    // Add random domain names
+                    dmn[rnd.Next(dmn.Count)]);
+            }
+
+            return await Task.FromResult(lst);
+        }
+    }
+}
